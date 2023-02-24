6ca1cd789ecf3018fe2927ec974d604ff807648c
Andreas Bigger <abigger87@gmail.com>
2023-02-24 16:53:07 -0700

    fix: snake casing

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -13,7 +13,7 @@ pub use fork::*;
 /// as defined in the [Optimism Exec Engine Specs](https://github.com/ethereum-optimism/optimism/blob/develop/specs/exec-engine.md).
 #[allow(non_snake_case)]
 pub trait L2EngineApi {
-    /// ## forkchoiceUpdated
+    /// ## forkchoice_updated
     ///
     /// Updates were made to [`engine_forkchoiceUpdatedV1`](https://github.com/ethereum/execution-apis/blob/main/src/engine/paris.md#engine_forkchoiceupdatedv1)
     /// for L2. This updates which L2 blocks the engine considers to be canonical ([ForkchoiceState] argument),
@@ -34,12 +34,12 @@ pub trait L2EngineApi {
     /// ### Reference
     ///
     /// See more details in the [Optimism Specs](https://github.com/ethereum-optimism/optimism/blob/develop/specs/exec-engine.md#engine_forkchoiceupdatedv1).
-    fn forkchoiceUpdated(
+    fn forkchoice_updated(
         forkchoiceState: ForkchoiceState,
         payloadAttributes: Option<PayloadAttributes>,
     ) -> Result<ForkChoiceUpdate>;
 
-    /// ## newPayload
+    /// ## new_payload
     ///
     /// No modifications to [`engine_newPayloadV1`](https://github.com/ethereum/execution-apis/blob/main/src/engine/paris.md#engine_newpayloadv1)
     /// were made for L2. Applies a L2 block to the engine state.
@@ -58,9 +58,9 @@ pub trait L2EngineApi {
     /// ### Reference
     ///
     /// See more details in the [Optimism Specs](https://github.com/ethereum-optimism/optimism/blob/develop/specs/exec-engine.md#engine_newPayloadv1).
-    fn newPayload(executionPayload: ExecutionPayload) -> Result<PayloadStatus>;
+    fn new_payload(executionPayload: ExecutionPayload) -> Result<PayloadStatus>;
 
-    /// ## getPayload
+    /// ## get_payload
     ///
     /// No modifications to [`engine_getPayloadV1`](https://github.com/ethereum/execution-apis/blob/main/src/engine/paris.md#engine_getpayloadv1)
     /// were made for L2. Retrieves a payload by ID, prepared by [engine_forkchoiceUpdatedV1](EngineApi::engine_forkchoiceUpdatedV1)
@@ -80,5 +80,5 @@ pub trait L2EngineApi {
     /// ### Reference
     ///
     /// See more details in the [Optimism Specs](https://github.com/ethereum-optimism/optimism/blob/develop/specs/exec-engine.md#engine_getPayloadv1).
-    fn getPayload(payloadId: PayloadId) -> Result<ExecutionPayload>;
+    fn get_payload(payloadId: PayloadId) -> Result<ExecutionPayload>;
 }
