7aa4527fffedccbe47695e1c642382296530cd6e
refcell.eth <abigger87@gmail.com>
2023-02-25 12:07:17 -0700

    feat: database backend (#14)
    * init: backend :web:
    
    * backend improvements
    
    * backend querying enhancements
    
    * init backend impl done

---
+++ src/backend/database.rs
@@ -0,0 +1,203 @@
+use eyre::Result;
+use std::collections::HashMap;
+use uuid::Uuid;
+
+use super::types::*;
+
+/// Memory backend to store blocks and transactions.
+#[derive(Debug, Clone)]
+pub struct Database {
+    /// A map of block hash to the block object.
+    blocks: HashMap<BlockHash, BlockNumber>,
+    /// A map from block number to block hash.
+    hashes: HashMap<BlockNumber, BlockHash>,
+    /// A map from transaction hash to the block (number) that contains it.
+    transactions: HashMap<TxHash, BlockNumber>,
+    /// Timestamp Mapping
+    timestamps: HashMap<Timestamp, Vec<BlockNumber>>,
+    /// L1 Origin Block Hash
+    l1_origin_block_hash: HashMap<BlockHash, Vec<BlockNumber>>,
+    /// L1 Origin Block Number
+    l1_origin_block_number: HashMap<BlockNumber, Vec<BlockNumber>>,
+    /// Internal [seld](sled) db
+    db: sled::Db,
+}
+
+impl Default for Database {
+    fn default() -> Self {
+        let loc = Self::fallback_location();
+        Self {
+            blocks: Default::default(),
+            hashes: Default::default(),
+            timestamps: Default::default(),
+            transactions: Default::default(),
+            l1_origin_block_hash: Default::default(),
+            l1_origin_block_number: Default::default(),
+            db: sled::open(loc).unwrap(),
+        }
+    }
+}
+
+impl Database {
+    /// Creates a new database.
+    pub fn new(loc: &str) -> Self {
+        Self {
+            db: Self::try_construct_db(loc),
+            ..Default::default()
+        }
+    }
+
+    /// Gets a random location to use as a fallback
+    pub fn fallback_location() -> String {
+        format!("/tmp/magi/{}", Uuid::new_v4())
+    }
+
+    /// Clear wipes a database sled location.
+    ///
+    /// ## Warning
+    ///
+    /// Be careful when using this function, as it will delete all data.
+    pub fn clear(&self) -> Result<()> {
+        self.db.clear().map_err(|e| eyre::eyre!(e))
+    }
+
+    /// Attempts to construct a database for a given location.
+    ///
+    /// ## Panics
+    ///
+    /// This function will panic if neither the given file location
+    /// nor a temporary location can be used to construct a database.
+    fn try_construct_db(loc: &str) -> sled::Db {
+        match sled::open(loc) {
+            Ok(db) => db,
+            Err(e) => {
+                tracing::error!("Failed to open database: {}", e);
+                let new_loc = Self::fallback_location();
+                tracing::debug!("Optimistically creating new database at {}", new_loc);
+                sled::open(new_loc).unwrap()
+            }
+        }
+    }
+
+    /// Returns the internal sled database.
+    pub fn inner(&self) -> &sled::Db {
+        &self.db
+    }
+
+    /// Returns the block number for a given block hash.
+    pub fn block_number(&self, hash: &BlockHash) -> Option<BlockNumber> {
+        self.blocks.get(hash).copied()
+    }
+
+    /// Returns the block hash for a given block number.
+    pub fn block_hash(&self, number: &BlockNumber) -> Option<BlockHash> {
+        self.hashes.get(number).copied()
+    }
+
+    /// Internal function to write a block to the database.
+    pub fn write_block(&mut self, block: ConstructedBlock) -> Result<()> {
+        let number = block.number;
+        if let Some(hash) = block.hash {
+            self.hashes.insert(number, hash);
+            self.blocks.insert(hash, number);
+        }
+        match self.timestamps.get(&block.timestamp) {
+            Some(v) => {
+                let mut v = v.clone();
+                v.push(number);
+                self.timestamps.insert(block.timestamp, v);
+            }
+            None => {
+                self.timestamps.insert(block.timestamp, vec![number]);
+            }
+        }
+        if let Some(l1_origin_block_hash) = block.l1_origin_block_hash {
+            match self.l1_origin_block_hash.get(&l1_origin_block_hash) {
+                Some(v) => {
+                    let mut v = v.clone();
+                    v.push(number);
+                    self.l1_origin_block_hash.insert(l1_origin_block_hash, v);
+                }
+                None => {
+                    self.l1_origin_block_hash
+                        .insert(l1_origin_block_hash, vec![number]);
+                }
+            }
+        }
+        if let Some(l1_origin_block_number) = block.l1_origin_block_number {
+            match self.l1_origin_block_number.get(&l1_origin_block_number) {
+                Some(v) => {
+                    let mut v = v.clone();
+                    v.push(number);
+                    self.l1_origin_block_number
+                        .insert(l1_origin_block_number, v);
+                }
+                None => {
+                    self.l1_origin_block_number
+                        .insert(l1_origin_block_number, vec![number]);
+                }
+            }
+        }
+        for tx in &block.transactions {
+            self.transactions.insert(tx.hash, number);
+        }
+        let dbid = DatabaseId::from(&block);
+        let ivec: sled::IVec = block.into();
+        let qid = dbid.as_bytes();
+        self.db.insert(qid, ivec)?;
+        Ok(())
+    }
+
+    /// Reads a block from cache, or the database.
+    pub fn read_block(&self, id: impl Into<DatabaseId>) -> Result<ConstructedBlock> {
+        let dbid = id.into();
+        let qid = dbid.as_bytes();
+        let block = self.db.get(qid)?;
+        block.map(Into::into).ok_or(eyre::eyre!("Block not found"))
+    }
+
+    /// Fetches a block with a given transaction hash.
+    pub fn block_by_tx_hash(&self, hash: TxHash) -> Option<ConstructedBlock> {
+        let block_number = self.transactions.get(&hash).copied()?;
+        self.read_block(block_number).ok()
+    }
+
+    /// Fetches blocks with a given timestamp.
+    pub fn blocks_by_timestamp(&self, timestamp: Timestamp) -> Vec<ConstructedBlock> {
+        let block_numbers = self
+            .timestamps
+            .get(&timestamp)
+            .cloned()
+            .unwrap_or(Vec::new());
+        block_numbers
+            .iter()
+            .filter_map(|&n| self.read_block(n).ok())
+            .collect()
+    }
+
+    /// Fetches blocks with a given L1 Origin Block Hash.
+    pub fn blocks_by_origin_hash(&self, hash: BlockHash) -> Vec<ConstructedBlock> {
+        let block_numbers = self
+            .l1_origin_block_hash
+            .get(&hash)
+            .cloned()
+            .unwrap_or(Vec::new());
+        block_numbers
+            .iter()
+            .filter_map(|&n| self.read_block(n).ok())
+            .collect()
+    }
+
+    /// Fetches blocks with a given L1 Origin Block Number.
+    pub fn blocks_by_origin_number(&self, number: BlockNumber) -> Vec<ConstructedBlock> {
+        let block_numbers = self
+            .l1_origin_block_number
+            .get(&number)
+            .cloned()
+            .unwrap_or(Vec::new());
+        block_numbers
+            .iter()
+            .filter_map(|&n| self.read_block(n).ok())
+            .collect()
+    }
+}
---
+++ src/backend/mod.rs
@@ -0,0 +1,35 @@
+#![warn(missing_debug_implementations)]
+#![deny(rustdoc::broken_intra_doc_links)]
+
+//! # Minimal Block Database
+//!
+//! This exposes a minimal block database that can be used to store and retrieve
+//! [ConstructedBlock](crate::types::ConstructedBlock) to persistent storage.
+//!
+//! ## Example
+//!
+//! ```rust
+//! use magi::backend::prelude::*;
+//!
+//! // Note: this will panic if both `/tmp/magi` and the hardcoded temporary location cannot be used.
+//! let mut db = Database::new("/tmp/magi");
+//! let mut block = ConstructedBlock::default();
+//! block.hash = Some(BlockHash::from_low_u64_be(1));
+//! db.write_block(block.clone()).unwrap();
+//! let read_block = db.read_block(block.hash.unwrap()).unwrap();
+//! assert_eq!(block, read_block);
+//! db.clear().unwrap();
+//! ```
+
+/// Core Backend Types
+mod types;
+pub use types::{BlockHash, BlockNumber, ConstructedBlock};
+
+/// Core Backend Database
+mod database;
+pub use database::Database;
+
+pub mod prelude {
+    pub use super::types::{BlockHash, BlockNumber, ConstructedBlock};
+    pub use super::Database;
+}
