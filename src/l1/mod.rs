6bee2d9a82f4ed0a2c80fae78e58bcb482da5e74
Noah Citron <noah@jeff.org>
2023-02-26 14:28:05 -0500

    feat: error handling in L1 watcher

--- src/l1/mod.rs
+++ src/l1/mod.rs
@@ -1,14 +1,15 @@
-use std::{sync::Arc, time::Duration};
+use std::{str::FromStr, sync::Arc, time::Duration};
 
 use ethers_core::{
     types::{Block, Filter, Transaction, H256},
     utils::keccak256,
 };
-use ethers_providers::{Middleware, Provider};
+use ethers_providers::{Http, HttpRateLimitRetryPolicy, Middleware, Provider, RetryClient};
 
+use eyre::Result;
 use tokio::{
     spawn,
-    sync::mpsc::{channel, Receiver},
+    sync::mpsc::{channel, Receiver, Sender},
     task::JoinHandle,
     time::sleep,
 };
@@ -22,6 +23,14 @@ pub struct ChainWatcher {
     pub deposit_receiver: Receiver<UserDeposited>,
 }
 
+struct InnerWatcher {
+    config: Arc<Config>,
+    provider: Provider<RetryClient<Http>>,
+    tx_sender: Sender<BatcherTransactionData>,
+    block_sender: Sender<Block<Transaction>>,
+    deposit_sender: Sender<UserDeposited>,
+}
+
 impl Drop for ChainWatcher {
     fn drop(&mut self) {
         self.handle.abort();
@@ -29,15 +38,15 @@ impl Drop for ChainWatcher {
 }
 
 impl ChainWatcher {
-    pub fn new(start_block: u64, config: Arc<Config>) -> Self {
-        let (handle, tx_receiver, block_receiver, deposit_receiver) =
-            chain_watcher(start_block, config);
-        Self {
+    pub fn new(start_block: u64, config: Arc<Config>) -> Result<Self> {
+        let (handle, receivers) = start_watcher(start_block, config)?;
+
+        Ok(Self {
             handle,
-            tx_receiver: Some(tx_receiver),
-            block_receiver,
-            deposit_receiver,
-        }
+            tx_receiver: Some(receivers.tx_receiver),
+            block_receiver: receivers.block_receiver,
+            deposit_receiver: receivers.deposit_receiver,
+        })
     }
 
     pub fn take_tx_receiver(&mut self) -> Option<Receiver<BatcherTransactionData>> {
@@ -45,93 +54,129 @@ impl ChainWatcher {
     }
 }
 
-fn chain_watcher(
-    start_block: u64,
-    config: Arc<Config>,
-) -> (
-    JoinHandle<()>,
-    Receiver<BatcherTransactionData>,
-    Receiver<Block<Transaction>>,
-    Receiver<UserDeposited>,
-) {
-    let (batcher_tx_sender, batcher_tx_receiver) = channel(1000);
-    let (block_sender, block_receiver) = channel(1000);
-    let (deposit_sender, deposit_receiver) = channel(1000);
+impl InnerWatcher {
+    fn new(
+        config: Arc<Config>,
+        tx_sender: Sender<BatcherTransactionData>,
+        block_sender: Sender<Block<Transaction>>,
+        deposit_sender: Sender<UserDeposited>,
+    ) -> Result<Self> {
+        let http = Http::from_str(&config.l1_rpc).map_err(|_| eyre::eyre!("invalid L1 RPC URL"))?;
+        let policy = Box::new(HttpRateLimitRetryPolicy);
+        let client = RetryClient::new(http, policy, 100, 50);
+        let provider = Provider::new(client);
 
-    let handle = spawn(async move {
-        let rpc_url = config.l1_rpc.clone();
-        let provider = Provider::try_from(rpc_url).unwrap();
+        Ok(Self {
+            config,
+            provider,
+            tx_sender,
+            block_sender,
+            deposit_sender,
+        })
+    }
 
-        let batch_sender = config.chain.batch_sender;
-        let batch_inbox = config.chain.batch_inbox;
-        let deposit_contract = config.chain.deposit_contract;
+    async fn try_ingest_block(&self, block_num: u64) -> Result<()> {
+        if !self.channels_full() {
+            let block = self.get_block(block_num).await?;
+            let deposits = self.get_deposits(block_num, block.hash.unwrap()).await?;
+            let batcher_transactions = self.get_batcher_transactions(&block);
 
+            self.block_sender.send(block).await?;
+
+            for deposit in deposits {
+                self.deposit_sender.send(deposit).await?;
+            }
+
+            for tx in batcher_transactions {
+                self.tx_sender.send(tx).await?;
+            }
+        } else {
+            sleep(Duration::from_millis(250)).await;
+        }
+
+        Ok(())
+    }
+
+    async fn get_block(&self, block_num: u64) -> Result<Block<Transaction>> {
+        self.provider
+            .get_block_with_txs(block_num)
+            .await?
+            .ok_or(eyre::eyre!("block not found"))
+    }
+
+    fn get_batcher_transactions(&self, block: &Block<Transaction>) -> Vec<BatcherTransactionData> {
+        block
+            .transactions
+            .iter()
+            .filter(|tx| {
+                tx.from == self.config.chain.batch_sender
+                    && tx
+                        .to
+                        .map(|to| to == self.config.chain.batch_inbox)
+                        .unwrap_or(false)
+            })
+            .map(|tx| tx.input.to_vec())
+            .collect()
+    }
+
+    async fn get_deposits(&self, block_num: u64, block_hash: H256) -> Result<Vec<UserDeposited>> {
         let deposit_event = "TransactionDeposited(address,address,uint256,bytes)";
         let deposit_topic = H256::from_slice(&keccak256(deposit_event));
 
         let deposit_filter = Filter::new()
-            .address(deposit_contract)
-            .topic0(deposit_topic);
+            .address(self.config.chain.deposit_contract)
+            .topic0(deposit_topic)
+            .from_block(block_num)
+            .to_block(block_num);
 
+        let deposit_logs = self.provider.get_logs(&deposit_filter).await?;
+
+        Ok(deposit_logs
+            .into_iter()
+            .map(|log| UserDeposited::from_log(log, block_num, block_hash).unwrap())
+            .collect())
+    }
+
+    fn channels_full(&self) -> bool {
+        self.tx_sender.capacity() == 0
+            || self.block_sender.capacity() == 0
+            || self.deposit_sender.capacity() == 0
+    }
+}
+
+struct Receivers {
+    tx_receiver: Receiver<BatcherTransactionData>,
+    block_receiver: Receiver<Block<Transaction>>,
+    deposit_receiver: Receiver<UserDeposited>,
+}
+
+fn start_watcher(start_block: u64, config: Arc<Config>) -> Result<(JoinHandle<()>, Receivers)> {
+    let (tx_sender, tx_receiver) = channel(1000);
+    let (block_sender, block_receiver) = channel(1000);
+    let (deposit_sender, deposit_receiver) = channel(1000);
+
+    let watcher = InnerWatcher::new(config, tx_sender, block_sender, deposit_sender)?;
+
+    let handle = spawn(async move {
         let mut block_num = start_block;
 
         loop {
-            tracing::debug!("fetching l1 data for block {}", block_num);
-
-            let channel_full = batcher_tx_sender.capacity() == 0
-                || block_sender.capacity() == 0
-                || deposit_sender.capacity() == 0;
-
-            if channel_full {
-                sleep(Duration::from_millis(250)).await;
-                continue;
+            tracing::debug!("fetching L1 data for block {}", block_num);
+            if watcher.try_ingest_block(block_num).await.is_ok() {
+                block_num += 1;
+            } else {
+                tracing::warn!("failed to fetch data for block {}", block_num);
             }
-
-            let block = provider
-                .get_block_with_txs(block_num)
-                .await
-                .unwrap()
-                .unwrap();
-
-            let batcher_txs = block.transactions.clone().into_iter().filter(|tx| {
-                tx.from == batch_sender && tx.to.map(|to| to == batch_inbox).unwrap_or(false)
-            });
-
-            let block_hash = block.hash.unwrap();
-
-            // blocks must be sent first to prevent stage from executing on a
-            // batch that we do not have the block for yet
-            block_sender.send(block).await.unwrap();
-
-            let filter = deposit_filter
-                .clone()
-                .from_block(block_num)
-                .to_block(block_num);
-
-            let deposit_logs = provider.get_logs(&filter).await.unwrap();
-
-            for deposit_log in deposit_logs {
-                let deposit = UserDeposited::from_log(deposit_log, block_num, block_hash).unwrap();
-                deposit_sender.send(deposit).await.unwrap();
-            }
-
-            for batcher_tx in batcher_txs {
-                batcher_tx_sender
-                    .send(batcher_tx.input.to_vec())
-                    .await
-                    .unwrap();
-            }
-
-            block_num += 1;
         }
     });
 
-    (
-        handle,
-        batcher_tx_receiver,
+    let receivers = Receivers {
+        tx_receiver,
         block_receiver,
         deposit_receiver,
-    )
+    };
+
+    Ok((handle, receivers))
 }
 
 type BatcherTransactionData = Vec<u8>;
