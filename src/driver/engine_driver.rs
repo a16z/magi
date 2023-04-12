c9066528af270d99286104444f2f4200a833ef9c
Noah Citron <noah@jeff.org>
2023-04-12 12:12:31 -0700

    prevent skip

--- src/driver/engine_driver.rs
+++ src/driver/engine_driver.rs
@@ -161,6 +161,8 @@ impl<E: Engine> EngineDriver<E> {
 }
 
 fn should_skip(block: &Block<H256>, attributes: &PayloadAttributes) -> Result<bool> {
+    return Ok(false);
+
     tracing::debug!(
         "comparing block at {} with attributes at {}",
         block.timestamp,
