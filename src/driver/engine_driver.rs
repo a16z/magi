a5101c82da6ad69e3f48f7cd4e282602f850cf37
Noah Citron <noah@jeff.org>
2023-04-12 10:00:04 -0700

    logging

--- src/driver/engine_driver.rs
+++ src/driver/engine_driver.rs
@@ -161,6 +161,8 @@ impl<E: Engine> EngineDriver<E> {
 }
 
 fn should_skip(block: &Block<H256>, attributes: &PayloadAttributes) -> Result<bool> {
+    tracing::debug!("comparing block at {} with attributes at {}", block.timestamp, attributes.timestamp);
+
     let attributes_hashes = attributes
         .transactions
         .as_ref()
