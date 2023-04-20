a4d0a9ba6fc0b362cccc76695b95ffb2f3c3d255
nicolas <48695862+merklefruit@users.noreply.github.com>
2023-04-20 19:55:51 +0200

    feat: store debug logs in files (#101)
    * feat: added tracing appender setup
    
    * fix: Asni -> Ansi typo
    
    * chore: small changes
    
    * chore: changed default rotation strat
    
    * chore: clippy

--- docker/start-magi.sh
+++ docker/start-magi.sh
@@ -10,4 +10,6 @@ exec magi \
     --l2-rpc-url http://op-geth:8545 \
     --l2-engine-url http://op-geth:8551 \
     --data-dir $DATADIR \
-    --sync-mode full
+    --sync-mode full \
+    --logs-dir $DATADIR/logs \
+    --logs-rotation never
