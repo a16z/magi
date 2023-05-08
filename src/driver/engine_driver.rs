f9b0bd100f4bd7c3019d655e26d9ead2027ceb18
Noah Citron <noah@jeff.org>
2023-05-08 15:59:10 -0400

    update forkchoice even when skipped

--- src/driver/engine_driver.rs
+++ src/driver/engine_driver.rs
@@ -38,7 +38,7 @@ impl<E: Engine> EngineDriver<E> {
 
         if let Some(block) = block {
             if should_skip(&block, &attributes)? {
-                self.skip_attributes(attributes, block)
+                self.skip_attributes(attributes, block).await
             } else {
                 self.unsafe_head = self.safe_head;
                 self.process_attributes(attributes).await
@@ -100,10 +100,11 @@ impl<E: Engine> EngineDriver<E> {
         Ok(())
     }
 
-    fn skip_attributes(&mut self, attributes: PayloadAttributes, block: Block<H256>) -> Result<()> {
+    async fn skip_attributes(&mut self, attributes: PayloadAttributes, block: Block<H256>) -> Result<()> {
         let new_epoch = *attributes.epoch.as_ref().unwrap();
         let new_head = BlockInfo::try_from(block)?;
         self.update_safe_head(new_head, new_epoch, false)?;
+        self.update_forkchoice().await?;
 
         Ok(())
     }
