fd79dfe3e5b7f1ce8ccb3e3323f934e7c62e6bac
nicolas <48695862+merklefruit@users.noreply.github.com>
2023-05-24 23:20:58 +0200

    wip: docker compose

--- docker/start-op-challenger.sh
+++ docker/start-op-challenger.sh
@@ -5,6 +5,8 @@ set -e
 DISPUTE_GAME_FACTORY=0x5a20cD16d4E51D3B2A6b2Ab460Ac735Fe421b820
 L2_OUTPUT_ORACLE=0xE6Dfba0953616Bacab0c9A8ecb3a9BBa77FC15c0
 
+echo "Starting op-challenger"
+
 exec op-challenger \
     --l1-ws-endpoint ws://${EXECUTION_CLIENT}:8546 \
     --trusted-op-node-endpoint http://magi:${RPC_PORT} \
